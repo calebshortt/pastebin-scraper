bb34631b3fa5d8f052393128b859241fc2862693
Caleb Shortt <cshorttrun@gmail.com>
2015-02-10 15:08:53 -0800

    Added setup tools and updated gitignore

---
+++ setup.py
@@ -0,0 +1,29 @@
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+
+# Grab the requirements using pip
+install_reqs = parse_requirements('requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
+
+
+__version__ = '1.0.0'
+
+setup(
+    name='Pastebin Password Scraper',
+    version=__version__,
+    description='A simple scraper of the public posts on pastebin. It looks for passwords.',
+    author='Caleb Shortt',
+    author_email='caleb@rgauge.com',
+    url='https://bitbucket.org/cshortt/pastebin-scraper/',
+    packages=find_packages(),
+    keywords=[
+        'pastebin',
+        'scraper'
+        'python',
+        'password',
+    ],
+    install_requires=reqs,
+)
+
